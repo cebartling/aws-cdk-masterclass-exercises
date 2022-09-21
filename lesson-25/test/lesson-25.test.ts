4029b7618e01808033c053a35350c3eb8d8f5bca
Christopher Bartling <chris.bartling@gmail.com>
2022-09-21 14:53:48 -0500

    feat: init of lesson 25

---
+++ lesson-25/test/lesson-25.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Lesson25 from '../lib/lesson-25-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lesson-25-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Lesson25.Lesson25Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
