75e0b7d99ab67d740f69c325ccf2ffec32a4f9c7
Christopher Bartling <chris.bartling@gmail.com>
2022-09-21 19:53:14 -0500

    feat: CDK code to create REST API

--- lesson-25/lib/lesson-25-stack.ts
+++ lesson-25/lib/lesson-25-stack.ts
@@ -1,16 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { join } from "path";
 
 export class Lesson25Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const getQuotesLambda = new Function(this, "getQuotesLambda", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(join(__dirname, "../lambdas")),
+      handler: "getQuotes.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Lesson25Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getQuotesRestApi = new RestApi(this, "getQuotesRestApi", {
+      description: "Quotes API",
+    });
+
+    const mainPath = getQuotesRestApi.root.addResource("quotes");
+    mainPath.addMethod("GET", new LambdaIntegration(getQuotesLambda));
   }
 }
